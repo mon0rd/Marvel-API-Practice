b832c16705a9ec55339478abafe48dc3bc75c5d1
mon0rd <monordus@gmail.com>
2025-11-15 09:04:54 +0200

    Fourteenth commit (React.lazy)

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
-import { StrictMode } from "react";
+import { StrictMode, lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import "/src/styles/index.sass";
 
 import Header from "/src/components/header/Header.jsx";
-import CharactersApp from "/src/components/pages/charactersApp/CharactersApp.jsx";
-import ComicsApp from "/src/components/pages/comicsApp/ComicsApp.jsx";
-// import ComicsItem from "/src/components/pages/comicsItem/ComicsItem.jsx";
-import Page404 from "/src/components/pages/404/404.jsx";
+// import CharactersApp from "/src/components/pages/charactersApp/CharactersApp.jsx";
+// import ComicsApp from "/src/components/pages/comicsApp/ComicsApp.jsx";
+// import Page404 from "/src/components/pages/404/404.jsx";
+import Spinner from "/src/components/spinner/Spinner.jsx";
 
+const CharactersApp = lazy(() =>
+  import("/src/components/pages/charactersApp/CharactersApp.jsx")
+);
+const ComicsApp = lazy(() =>
+  import("/src/components/pages/comicsApp/ComicsApp.jsx")
+);
+const Page404 = lazy(() => import("/src/components/pages/404/404.jsx"));
 // import MarvelService from "/src/services/MarvelService.jsx";
 // const marvelService = new MarvelService();
 // marvelService.getCharacter(1011005).then((res) => console.log(res));
@@ -18,12 +25,22 @@ createRoot(document.getElementById("root")).render(
     <div className="wrapper">
       <BrowserRouter>
         <Header />
-        <Routes>
-          <Route path="/" element={<CharactersApp />} />
-          <Route path="/comics/*" element={<ComicsApp />} />
-          {/* <Route path="/comics/:comicsId" element={<ComicsItem />} /> */}
-          <Route path="*" element={<Page404 />} />
-        </Routes>
+        <Suspense
+          fallback={
+            <Spinner
+              style={{
+                width: "188px",
+                height: "188px",
+                margin: "100px  auto",
+              }}
+            />
+          }>
+          <Routes>
+            <Route path="/" element={<CharactersApp />} />
+            <Route path="/comics/*" element={<ComicsApp />} />
+            <Route path="*" element={<Page404 />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </div>
   </StrictMode>
