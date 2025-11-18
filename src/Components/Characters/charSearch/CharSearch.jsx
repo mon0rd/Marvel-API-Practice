2056392cbe2748bc3560c9c4b2143caa54bfee55
mon0rd <monordus@gmail.com>
2025-11-18 12:33:23 +0200

    Seventeenth commit (CharSearch form validation and styling)

--- src/Components/Characters/charSearch/CharSearch.jsx
+++ src/Components/Characters/charSearch/CharSearch.jsx
@@ -1,53 +1,101 @@
 import { useState } from "react";
 import "/src/components/characters/charSearch/CharSearch.sass";
 import useMarvelService from "/src/services/MarvelService.jsx";
-import { useForm, Form } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import Spinner from "/src/components/spinner/Spinner.jsx";
 
 const CharSearch = (props) => {
   const [char, setChar] = useState(null);
-  const { getCharacterByName } = useMarvelService();
-  const { register, handleSubmit } = useForm();
+  const { error, clearError, expanding, setExpanding, getCharacterByName } =
+    useMarvelService();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
   const onSubmit = (data) => {
-    searchChar(
-      data.character.toLowerCase().replace(/\b\w/g, (c) => c.toUpperCase())
-    );
+    setExpanding(true);
+    setChar(null);
+    if (data.character) {
+      searchChar(
+        data.character.toLowerCase().replace(/\b\w/g, (c) => c.toUpperCase())
+      );
+    }
   };
 
   const searchChar = async (character) => {
-    getCharacterByName(character).then(onCharFind);
+    getCharacterByName(character)
+      .then(onCharFind)
+      .catch(() => setExpanding(false));
   };
 
   const onCharFind = (newchar) => {
     setChar(newchar);
-    // console.log(char);
+    setExpanding(false);
+  };
+
+  const onFieldChange = () => {
+    setChar(null);
+    clearError();
   };
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)} className="CharForm">
-        <label className="title_h3">Or find a character by name:</label>
-        <div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        onChange={onFieldChange}
+        className="CharSearch">
+        <label htmlFor="name" className="title_h3">
+          Or find a character by name:
+        </label>
+        <div className="CharSearch_wrapper">
           <input
-            {...register("character")}
-            className="CharForm_input"
+            {...register("character", { required: true })}
+            aria-invalid={errors.character ? "true" : "false"}
+            className="CharSearch_input"
+            id="name"
             placeholder="Enter name"
           />
-          <button type="submit" className="red_btn">
-            find
-          </button>
-        </div>
-        {char ? (
-          <div>
-            <div className="CharForm_error">There is! Visit ${name} page?</div>
-            <button
-              onClick={() => props.onCharSearch(char)}
-              className="gray_btn">
-              to page
+          {expanding ? (
+            <Spinner
+              style={{
+                margin: "auto auto auto 55px",
+                "--bar-thickness": "2px",
+                height: "38px",
+                width: "38px",
+              }}
+            />
+          ) : (
+            <button type="submit" className="red_btn">
+              find
             </button>
-          </div>
-        ) : null}
+          )}
+        </div>
+
+        <div className="CharSearch_wrapper">
+          {errors.character?.type === "required" && (
+            <p className="CharSearch_status_red">This field is required</p>
+          )}
+          {error && (
+            <p className="CharSearch_status_red">
+              The character was not found. Check the name and try again
+            </p>
+          )}
+
+          {char ? (
+            <>
+              <div className="CharSearch_status_green">
+                There is! Visit {char.name} page?
+              </div>
+              <button
+                onClick={() => props.onCharSearch(char)}
+                className="gray_btn">
+                to page
+              </button>
+            </>
+          ) : null}
+        </div>
       </form>
     </>
   );
